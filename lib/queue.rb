cbaff6e7a7a8dd73af5656de1093a1c2f14d08e9
Ryan Travitz <rtravitz@gmail.com>
2016-09-04 18:40:23 -0600

    added clear and count for queue

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,11 +1,15 @@
 class Queue
   attr_accessor :data
-  
+
   def initialize
     @data = Array.new
   end
 
   def clear
     @data = Array.new
+  end
+
+  def count
+    @data.count
   end
 end
--- test/queue_test.rb
+++ test/queue_test.rb
@@ -1,5 +1,6 @@
 require "./test/test_helper"
 require "./lib/queue"
+require "./lib/event_reporter"
 
 class QueueTest < Minitest::Test
 
@@ -9,5 +10,24 @@ class QueueTest < Minitest::Test
     assert_instance_of Queue, queue
   end
 
+  def test_queue_returns_correct_count
+    reporter = EventReporter.new
+    reporter.load("./data/short_attendees.csv")
+    reporter.find("first_name sarah")
+
+    assert_equal 2, reporter.queue.count
+  end
+
+  def test_clear_empties_the_queue
+    reporter = EventReporter.new
+    reporter.load("./data/short_attendees.csv")
+    reporter.find("first_name sarah")
+
+    assert_equal 2, reporter.queue.count
+
+    reporter.queue.clear
+
+    assert_equal 0, reporter.queue.count
+  end
 
 end
