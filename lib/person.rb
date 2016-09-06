23f59eeaec97794fc4024acb812dac0fea5d11aa
Ryan Travitz <rtravitz@gmail.com>
2016-09-06 16:26:57 -0600

    working person class

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  attr_accessor :first_name, :last_name, :regdate, :email_address, :homephone, :street, :city, :state, :zipcode
+  def initialize(row)
+    @first_name = row[:first_name]
+    @last_name = row[:last_name]
+    @regdate = row[:regdate]
+    @email_address = row[:email_address]
+    @homephone = row[:homephone]
+    @street = row[:street]
+    @city = row[:city]
+    @state = row[:state]
+    @zipcode = row[:zipcode]
+  end
+end
